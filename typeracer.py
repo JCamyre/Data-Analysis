b1421c491cd00c8b4d2cb0e6c0ca4e24e20ea57c
JCamyre <JWcamry03@gmail.com>
2021-01-31 12:08:11 -0800

    Added method to retrieve soup from a typeracer user's race history.

---
+++ typeracer.py
@@ -0,0 +1,17 @@
+from requests import get 
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+
+def get_history(username):
+	url = f'https://data.typeracer.com/pit/race_history?user={username}&n=100&startDate='
+	headers = {'User-Agent': "'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 "
+						 "(KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'"}
+	response = get(url, headers=headers, timeout=20)
+	assert response.status_code == 200 # Test to see if we received data
+	soup = BeautifulSoup(response.content, 'lxml')
+	return soup
+
+print(get_history('itypesomewhatalot'))
+
+def graph_history():
+	pass
